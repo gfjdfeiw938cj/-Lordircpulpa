2453cb006dd5b6887ad69fda8ca404dd19797ddc
gfjdfeiw938cj <82382819+gfjdfeiw938cj@users.noreply.github.com>
2021-04-17 11:25:13 +0300

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -2,29 +2,35 @@ const express = require('express')
 const app = express()
 
 const childProcess = require('child_process')
-const scriptFolder = 'C:\\Users\\Артур\\все фаилы Node.js\\archive_file_new.js'
+const scriptPath = 'C:\\Users\\Артур\\все фаилы Node.js\\archive_file_new.js'
 
-app.post('/process', function (req, res) {
-    function runScript(scriptFail, callback) {
-        let Uslov = false
-        let process = childProcess.fork(scriptFail);
-        process.on('error', function (err) {
-            if (Uslov) return;
-            Uslov = true
-            callback(err)
-        })
-        process.on('exit', function (code) {
-            if (Uslov) return
-            Uslov = true
-            let err = code === 0 ? null : new Error('exit code ' + code)
-            callback(err)
-        })
-    }
-    runScript(scriptFolder, function (err) {
-    if (err) throw err;
-    console.log('Скрипт выполнился');
+function runScript(scriptFail, callback) {
+ 
+    let flag = false
+    let process = childProcess.fork(scriptFail);
+    process.on('error', err => {
+        if (flag) return;
+        flag = true
+        callback(err)
     })
-    res.send("Скрипт выполнился")
+    process.on('exit', code => {
+        if (flag) return
+        flag = true
+        let err = code === 0 ? null : new Error('exit code ' + code)
+        callback(err)
+    })  
+}
+app.post('/process', function (req, res) {
+    try{
+        runScript(scriptPath , err => {
+            if(err) console.log('Ошибка в scriptPath')
+            else console.log('Скриит успешно выпонен')
+        })     
+        res.send("Скрипт выполнился")
+    }
+    catch(err){
+            console.log(' В Скрипте Task1 допушена ошибка')
+    }
 })
 app.listen(3000, function () {
     console.log('Сервер был запушен !')
