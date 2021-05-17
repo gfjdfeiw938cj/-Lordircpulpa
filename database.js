474c631ba7063f7f4f93cf0d111da3a9952ac3e6
gfjdfeiw938cj <82382819+gfjdfeiw938cj@users.noreply.github.com>
2021-05-17 15:08:51 +0300

    1111
    1111

--- database.js
+++ database.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
      
     let thingss = require('./script')
        
@@ -9,40 +8,11 @@
             return {
                 name:el[0],
                 process: el[1],
-=======
-        
-        
-
-
-       
-      let thingssss = require('./script')
-      module.exports.base = base
-
-      function base () {       
-        let ID = function () {  
-            return '_' + Math.random().toString(36).substr(2, 9);
-          };
-
-        let points = [[thingssss.scriprs1() , "process 1",ID()],[thingssss.scriprs1(),"process 2",ID()]];
-        let myArray = points.map( function(el){
-            return {
-                process:el[0],
-                name: el[1],
->>>>>>> 63d3e1f60cbfeb2128fe2f309c066b7e968604f4
                 ID: el[2]
         };
         
     });
     
-<<<<<<< HEAD
     let arr = JSON.stringify(myArray);
     return arr
 }
-=======
-    let object = JSON.stringify(myArray);
-    console.log(object)
-    return object
-}
-
-
->>>>>>> 63d3e1f60cbfeb2128fe2f309c066b7e968604f4
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 
 
 module.exports.task1 = function archive_files(){
@@ -8,22 +7,10 @@ module.exports.task1 = function archive_files(){
     
     let flag = false
     let process = childProcess.fork('C:\\Users\\Артур\\все фаилы Node.js\\archive_file_new.js' , [1] )
-=======
-module.exports.scriprs1 = scriprs1
-
-function scriprs1 (){
-   let fun = {
-    scriprs(scriptFile, callback){
-    const childProcess = require('child_process')
-    
-    let flag = false
-    let process = childProcess.fork(scriptFile , [1] )
->>>>>>> 63d3e1f60cbfeb2128fe2f309c066b7e968604f4
    
     process.on('close', (code) =>{
        `Дочерний процесс вышел ${code}`
     })
-<<<<<<< HEAD
     process.on('message', (code) =>{
         console.log(`Message to parent: ${code}`)
     })
@@ -31,8 +18,6 @@ function scriprs1 (){
         console.log('Files list: \n', data)
      })
 
-=======
->>>>>>> 63d3e1f60cbfeb2128fe2f309c066b7e968604f4
     process.on('error', err => {
         if (flag) return;
         flag = true
@@ -44,34 +29,5 @@ function scriprs1 (){
         let err = code === 0 ? null : new Error('exit code ' + code)
         callback(err)
     }) 
-<<<<<<< HEAD
 }
 
-=======
-  }
-}
-return fun
-}
-
-// function base () {       
-//     let ID = function () {  
-//         return '_' + Math.random().toString(36).substr(2, 9);
-//       };
-
-//     let points = [[thingssss.scriprs1 , "process 1",ID()],[thingssss.scriprs1,"process 2",ID()]];
-//     let myArray = points.map( function(el){
-//         return {
-//             process:el[0],
-//             name: el[1],
-//             ID: el[2]
-//     };
-    
-// });
-
-// let object = JSON.stringify(myArray);
-// console.log(object)
-// return object
-// }
-
-// base()
->>>>>>> 63d3e1f60cbfeb2128fe2f309c066b7e968604f4
