0f5e1d656a99cd28d42487ea9adf1723baafde34
gfjdfeiw938cj <82382819+gfjdfeiw938cj@users.noreply.github.com>
2021-05-31 12:50:08 +0300

    11
    11

--- server.js
+++ server.js
@@ -2,35 +2,28 @@
 const express = require('express')
 const app = express()
 const jsonParser = express.json();
-let things2 = require('./script')
 const fs = require("fs");
 app.use(express.static(__dirname + "/public"));
 
-const scriptPath = [ 'C:\\Users\\Артур\\все фаилы Node.js\\archive_file_new.js']
+const scriptPathg = 'C:\\Users\\Артур\\все фаилы Node.js\\archive_file_new.js'
 
-const filePath = "obj3.json";
-
-let scriptFile = things2.task1
-
+const filePath = "dataBase.json";
+ 
   app.get("/process", function(req, res){
        
     const content = fs.readFileSync(filePath,"utf8");
     const jsonDatabase = JSON.parse(content);
     res.send(jsonDatabase);
     });
+
   app.get("/process/:id", function(req, res){
        
     const id = req.params.id
     const content = fs.readFileSync(filePath, "utf8")
     const jsonDatabase = JSON.parse(content)
-    let arrEl = null
- 
-    for(let i=0; i<jsonDatabase.length; i++){
-        if(jsonDatabase[i].id==id){
-            arrEl = jsonDatabase[i];
-            break;
-        }
-    }
+   
+   let arrEl =  jsonDatabase.find(el => el.id==id )  
+
     if(arrEl){
         res.send(arrEl);
     }
@@ -41,62 +34,56 @@ let scriptFile = things2.task1
 
   app.post('/process',jsonParser, function (req, res) {
     try{
-
-    const nameScript  = req.body.name = scriptFile.name  
-    const scriptPath = req.body.script = scriptFile
+      req.body.name = 'archive_file_new'
+      req.body.script = scriptPathg
+      console.log(req.body)
     
-    let arrEl = {name: nameScript ,script: scriptPath}
-    let data = fs.readFileSync(filePath, "utf8")
-    let jsonDatabase = JSON.parse(data)
-
-    const id= Math.max.apply(Math,jsonDatabase.map(function(el){return el.id}))
+      let arrEl = {name: req.body.name ,script: req.body.script}
+      
+      let data = fs.readFileSync(filePath, "utf8")
+      let jsonDatabase = JSON.parse(data)
+     
+      const id = Math.max.apply(Math,jsonDatabase.map(el => el.id))
         
-    arrEl.id = id+1;
+      arrEl.id = id+1;
    
-    jsonDatabase.push(arrEl)
+      jsonDatabase.push(arrEl)
 
-    data = JSON.stringify(jsonDatabase, function(k,v){
-        if(typeof v === "function")
-          return v.toString();
-        return v;
-    });
+      data = JSON.stringify(jsonDatabase)
+      fs.writeFileSync(filePath, data)
 
-    fs.writeFileSync(filePath, data)
-   
-    scriptFile(scriptPath , err => {
-        if(err) console.log('Ошибка в scriptPath')
+      const process = require("child_process").fork(req.body.script)
+      process.on('error', err => {
+        if(err)console.log(` В Скрипте Task1 допушена ошибка ${err}`)
         else console.log('Скрипт успешно выполнился')
-    })     
-        res.send(`Скрипт выполнился ${scriptFile.name}`)
-
-    }
-    catch(err){
-        console.log(' В Скрипте Task1 допушена ошибка')
-        res.send(` В Скрипте Task1 допушена ошибка `)
+      })
+      process.on('Exit', code => {
+        console.log(`код выхода: ${code}`)
+      })
+      res.send( req.body)
+      }
+      catch(err){
+        console.log(' В Скрипте server допушена ошибка')
+        res.send(` В Скрипте server допушена ошибка `)
     }
   });
   
-  app.put("/process", jsonParser, function(req, res){
-        
-    const scriptId = req.body.id = 5 // <= введите свой id
-    const scriptName = req.body.name = 'введите свои значение'
-    const script = req.body.script = 'введите свои значения'
-        
-    let data = fs.readFileSync(filePath, "utf8")
-    const jsonDatabase = JSON.parse(data)
-    let arrEl
-    for(let i=0; i<jsonDatabase.length; i++){
-        if(jsonDatabase[i].id==scriptId){
-            arrEl = jsonDatabase[i];
-            break;
-        }
-    }
-        if(arrEl){
-            arrEl.name  = scriptName;
-            arrEl.script = script;
-            data = JSON.stringify(jsonDatabase);
-            fs.writeFileSync(filePath, data);
-            res.send(arrEl);
+  app.put("/process/:id/:name/:script", jsonParser, function(req, res){
+   
+      const id = req.params.id;  
+      const name = req.params.name;
+      const script = req.params.script;
+      let data = fs.readFileSync(filePath, "utf8")
+      const jsonDatabase = JSON.parse(data)
+    
+      let arrEl =  jsonDatabase.find(el => el.id == id)
+    
+      if(arrEl){
+        arrEl.name = name;
+        arrEl.script = script;
+        data = JSON.stringify(jsonDatabase);
+        fs.writeFileSync(filePath, data);
+        res.send(jsonDatabase);
         }
       else{
           res.status(404).send(arrEl);
@@ -105,29 +92,22 @@ let scriptFile = things2.task1
 
   app.delete("/process/:id", function(req, res){
        
-    const id = req.params.id;
-    let data = fs.readFileSync(filePath, "utf8");
-    let jsonDatabase = JSON.parse(data);
-    let index = -1;
-    // находим индекс пользователя в массиве
-    for(let i=0; i < jsonDatabase.length; i++){
-        if(jsonDatabase[i].id==id){
-            index=i;
-            break;
-        }
-    }
-    if(index > -1){
-        const arrEl = jsonDatabase.splice(index, 1)[0];
+      const id = req.params.id;
+      let data = fs.readFileSync(filePath, "utf8");
+      let jsonDatabase = JSON.parse(data);
+      let index = jsonDatabase.indexOf(jsonDatabase.find(el => el.id==id))
+
+    if(index > -1 ){
+        jsonDatabase.splice(index, 1)[0];
         data = JSON.stringify(jsonDatabase);
-        fs.writeFileSync("users.json", data);
-        res.send(arrEl);
+        fs.writeFileSync(filePath , data);
+        res.send(jsonDatabase)
     }
     else{
-        res.status(404).send();
+      setTimeout(() => res.status(403).end('Процес был принудительно остановлен'), 1000);
     }
-});
+  });
 
   app.listen(3000, function () {
     console.log('Сервер был запушен !')
-})
-
+  })
